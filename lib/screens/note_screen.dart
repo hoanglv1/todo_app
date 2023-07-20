95b012ddf49e73e8b52a6425dc54fcc81d0457d2
hoanglv <hoanglv1@hblab.vn>
2023-07-20 16:03:51 +0700

    exxport PDF file

--- lib/screens/note_screen.dart
+++ lib/screens/note_screen.dart
@@ -1,8 +1,12 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:note_app/data/dummy_data.dart';
 import 'package:note_app/database/todo_database.dart';
 import 'package:note_app/model/note_model.dart';
 import 'package:note_app/widgets/color_table.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:pdf/widgets.dart' as pw;
 
 class NoteScreen extends StatefulWidget {
   NoteScreen({super.key, this.refreshCallback, required this.mode, this.note});
@@ -38,6 +42,12 @@ class _NewNote extends State<NoteScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        title: Text(
+            widget.mode == "edit"
+                ? widget.note!.dateTimeDay
+                : "Creating New Note",
+            style: TextStyle(color: Colors.black)),
+        centerTitle: true,
         backgroundColor:
             widget.mode == "edit" ? colorsAppBar[noteColor] : colorAppBar,
         actions: widget.mode == "edit"
@@ -49,7 +59,7 @@ class _NewNote extends State<NoteScreen> {
                       Navigator.pop(context);
                     },
                     icon: const Icon(Icons.delete))
-              ]
+        ]
             : null,
         iconTheme: const IconThemeData(color: Colors.black),
       ),
@@ -74,42 +84,40 @@ class _NewNote extends State<NoteScreen> {
           ),
         ),
         Container(
+          height: 80,
           color: widget.mode == "edit" ? colorsBody[noteColor] : colorBody,
           child: Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              IconButton(
-                  onPressed: () {},
-                  icon: const Icon(Icons.format_bold, size: 30)),
-              IconButton(
-                  onPressed: () {},
-                  icon: const Icon(Icons.format_italic, size: 30)),
-              IconButton(
-                  onPressed: () {},
-                  icon: const Icon(Icons.format_underline_outlined, size: 30)),
-              IconButton(
-                  onPressed: () {},
-                  icon: const Icon(Icons.format_list_bulleted, size: 30)),
-              IconButton(
-                  onPressed: () {}, icon: const Icon(Icons.image, size: 30)),
+              const SizedBox(width: 10),
               IconButton(
                   onPressed: () {
                     _changeNoteColorBackground();
                   },
-                  icon: const Icon(Icons.color_lens_sharp, size: 30)),
+                  icon: const Icon(Icons.color_lens_sharp, size: 40)),
+              const SizedBox(width: 10),
+              IconButton(
+                  onPressed: () {
+                    if (widget.mode == "edit") {
+                      _exportToPdf();
+                    }
+                  },
+                  icon: const Icon(Icons.picture_as_pdf_outlined, size: 40)),
               const Spacer(),
               IconButton(
                   onPressed: () {
-                    _submitNote(noteTitle, noteBody);
+                    _submitNote();
                   },
-                  icon: const Icon(Icons.done, size: 30)),
+                  icon: const Icon(Icons.done, size: 40)),
+              const SizedBox(width: 10),
             ],
           ),
-        )
+        ),
       ]),
     );
   }
 
-  _submitNote(String noteTitle, String noteBody) {
+  _submitNote() {
     // Handle Note Data
     String noteContent = _noteContentController.text.trim();
     if (noteContent.isEmpty) {
@@ -117,13 +125,13 @@ class _NewNote extends State<NoteScreen> {
       return;
     }
     List<String> noteLines = noteContent.split('\n');
-    noteTitle = noteLines.first;
+    var noteTitle = noteLines.first;
     // Add Note Data
     if (widget.mode == "new") {
       var newNote = NoteModel(
           null, noteTitle, noteContent, DateTime.now(), colorPosition);
-      database.insertNotes(
-          newNote.title, newNote.noteBody, newNote.dateTimeDay, 0);
+      database.insertNotes(newNote.title, newNote.noteBody, newNote.dateTimeDay,
+          newNote.noteColor);
     } else {
       database.updateNoteById(
           widget.note!.id!, noteTitle, noteContent, noteColor);
@@ -155,5 +163,62 @@ class _NewNote extends State<NoteScreen> {
         }
       });
     }
+  }
+
+  Future<void> _exportToPdf() async {
+    final pdf = pw.Document();
+
+    // Get the text from the TextField
+    String text = _noteContentController.text;
+
+    // Create a paragraph with the text
+    pdf.addPage(
+      pw.Page(
+        build: (pw.Context context) {
+          return pw.Column(
+              crossAxisAlignment: pw.CrossAxisAlignment.start,
+              mainAxisAlignment: pw.MainAxisAlignment.start,
+              children: [
+                pw.Text('Title',
+                    style: pw.TextStyle(
+                        fontWeight: pw.FontWeight.bold, fontSize: 24)),
+                pw.DecoratedBox(decoration: BoxDecoration(
+                  border: Border(bottom: BorderSide(width: 1, color: Colors.black)),
+                ),),
+                pw.SizedBox(height: 10),
+                pw.Text(text)
+              ]);
+        },
+      ),
+    );
+
+    // Get the document directory path
+    Directory appDocDir = await getApplicationDocumentsDirectory();
+    String appDocPath = appDocDir.path;
+
+    // Create the PDF file
+    File file = File('$appDocPath/example.pdf');
+
+    // Write the PDF content to the file
+    await file.writeAsBytes(await pdf.save());
+
+    // Show a dialog with the path to the PDF file
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text("Export to PDF"),
+          content: Text("PDF file saved to: $appDocPath/example.pdf"),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: Text("OK"),
+            ),
+          ],
+        );
+      },
+    );
   }
 }
